182a351415183cd9214179920e3753875ac5734b
Hugo Perez Garcia <hugoperez80@hotmail.com>
2022-02-09 11:28:11 +0100

    featRoutes

---
+++ src/pages/Buscador/Buscador.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Buscador = () => {
+  return <div>
+      Hola soy el Buscador
+  </div>;
+};
+
+export default Buscador;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Home.scss';
+
 const Home = () => {
   return (
     <div className="home-container">
       <img src="logoHome" alt="" />
       <h1>Applergic</h1>
       <p>Mi guía alimentaria</p>
-      <button>Escanear</button>
+     <Link className="QR" to='/Home/Escanear'><button>Escanear</button></Link> 
       <p>Escanea un nuevo producto</p>
-      <button>Buscar</button>
+      <Link className="Buscador" to='/Home/Buscador'><button>Buscar</button></Link>
       <p>Busca un comercio o restaurante para ti</p>
-      <button>S.O.S</button>
+     <Link className="Emergencias" to='/Home/Emergencias'> <button>S.O.S</button></Link>
       <p>¿Necesitas ayuda urgente? contactamos con emergencias</p>
     </div>
   );
