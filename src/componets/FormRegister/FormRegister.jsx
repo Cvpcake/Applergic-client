88d4809dc541ec6333e7052a63d24ed402cef214
unknown <yoel.arenas26@gmail.com>
2022-02-10 11:23:24 +0100

    feat Form send

--- src/componets/FormRegister/FormRegister.jsx
+++ src/componets/FormRegister/FormRegister.jsx
@@ -1,51 +1,48 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { useForm } from "react-hook-form";
 import { API } from "../../shared/services/api";
 import './FormRegister.scss';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Scrollbar } from 'swiper';
-import {SlideNextButton, SlidePrevButton} from "../SlideNextButton/SlideNextButton";
+import {SlideNextButton1, SlideNextButton2, SlideNextButton3, SlidePrevButton} from "../SlideNextButton/SlideNextButton";
 import "swiper/css";
 
-
-
-let valueCont = 0
+// let valueCont = 0
 
 export default function RegisterPage () {
     const { register, handleSubmit } = useForm();
-    const [counter, setCounter] = useState(valueCont)
-    // counter = 0;
 
-
-    const onSubmit = (formData) => {
+    const onSubmit = formData => {
+        console.log(register)
+        console.log(formData)
         API.post('users/', formData).then(res => {
             console.log('Register user',)
             console.log(res);
             console.log(formData);
-        })
-    }
+        })}
 
-    useEffect(()=>{
-        valueCont = 0;
-            },[])
+// useEffect( ()=>{
+    
+// },[])
     
 
     return (
+        
+    <form className="formRegister" onSubmit={handleSubmit(onSubmit)}>
+            
+            {/* SOLUCIONAR...xd */}
+            {/* <label htmlFor="image">Imagen</label>
+            <input type="file" id="image" 
+                   {...register("file")} /> */}
         <Swiper
         modules={[Pagination,Navigation,Scrollbar]}
         spaceBetween={50}
         slidesPerView={1}
         allowTouchMove={false}
         className="swiperForm"
-      >
-        <form className="formRegister" onSubmit={handleSubmit(onSubmit)}>
-            
-            {/* SOLUCIONAR...xd */}
-            {/* <label htmlFor="image">Imagen</label>
-            <input type="file" id="image" 
-                   {...register("file")} /> */}
+        >
                    
-            <SwiperSlide>
+        <SwiperSlide>
             
             <label htmlFor="name">Nombre</label>
             <input id="name" defaultValue="Luisa María de las Nieves"
@@ -66,16 +63,16 @@ export default function RegisterPage () {
                        required: true,
                     //    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
                    })}/>
-            </SwiperSlide>
+            <SlideNextButton1/>
+        </SwiperSlide> 
             
-            <SwiperSlide>
-
+        <SwiperSlide>
                 <label htmlFor="nameContact" >Nombre Completo de tu contacto</label>
                 <input id="nameContact" defaultValue="Jorge de las Nieves"
                     {...register("nameContact", { required: true })}/>
 
                 <label htmlFor="emailContact">Email del contacto</label>
-                <input id="emailContact" defaultValue="luisam@delasnieves.com"
+                <input id="emailContact" defaultValue="jorge@delasnieves.com"
                     {...register("emailContact", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}/>
 
                 <label htmlFor="emergencyContact">Movil del contacto</label>
@@ -85,36 +82,34 @@ export default function RegisterPage () {
                 <label htmlFor="SecureCompany">Compañía de Seguro del contacto/ Nº de Póliza</label>
                 <input id="SecureCompany" defaultValue="Mutua? idk xd"
                     {...register("SecureCompany", { required: true })}/>
-            
-            
-                
-            </SwiperSlide>
-            <SwiperSlide>
-            <input id="allergies" type="checkbox"
-                    {...register("allergies")}/>
-            
-            <input id="allergies" type="checkbox"
-                    {...register("allergies")}/>
-                
-            <input id="allergies" type="checkbox"
-                    {...register("allergies")}/>
-            
-             <input id="allergies" type="checkbox"
-                    {...register("allergies")}/>
-            
-            {counter === 3 ? <SlideNextButton counter={counter} setCounter={setCounter}/> : <></> }
-            {console.log('e entrado, counter es: ',counter)}
-            </SwiperSlide>
+                <SlideNextButton2 />
+        </SwiperSlide>
 
-            <SwiperSlide>
+        <SwiperSlide>
+            <input id="Huevo" type="checkbox" defaultValue="Huevo"
+                    {...register("allergies")}/>
+            
+            <input id="Gluten" type="checkbox"
+                    {...register("allergies")}/>
+                
+            <input id="Lacteos" type="checkbox"
+                    {...register("allergies")}/>
+            
+             <input id="Cacao" type="checkbox"
+                    {...register("allergies")}/>
+            <SlideNextButton3 />
+        </SwiperSlide> 
+
+        <SwiperSlide>
             <h2>CONFIRMAR TU SELECCIÓN</h2>
-            </SwiperSlide>
-            
-            
-            
-        </form>
-        <SlideNextButton counter={counter} setCounter={setCounter}/>
-        <SlidePrevButton counter={counter} setCounter={setCounter} />
+            <SlidePrevButton/>
+            <input type="submit" value="CONFIRMAR"/>
+        </SwiperSlide>
+
         </Swiper>
+    </form>
+    
+        
+        
     )
 }
